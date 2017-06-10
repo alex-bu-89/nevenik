311a624782126b360cee940ebc502ea9b4901870
Alex Buyanov <alex.bu.89@gmail.com>
2017-06-10 11:58:11 +0200

    fix: fix footer, header, small css fixes, turn off slider from template

--- wp-content/themes/kupinevenik/inc/storefront-template-hooks.php
+++ wp-content/themes/kupinevenik/inc/storefront-template-hooks.php
@@ -35,7 +35,7 @@ add_action( 'storefront_header', 'storefront_primary_navigation_wrapper_close', 
  * @see  storefront_footer_widgets()
  * @see  storefront_credit()
  */
-add_action( 'storefront_footer', 'storefront_footer_widgets', 10 );
+// add_action( 'storefront_footer', 'storefront_footer_widgets', 10 );
 add_action( 'storefront_footer', 'storefront_credit',         20 );
 
 /**
